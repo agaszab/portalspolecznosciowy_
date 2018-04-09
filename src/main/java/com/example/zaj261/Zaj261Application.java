e74210e82175db3d2f40163cdc2a3897979b3a23
Agnieszka <gatito@poczta.pl>
2018-04-09 08:31:31 -0700

    prog

---
+++ src/main/java/com/example/zaj261/Zaj261Application.java
@@ -0,0 +1,12 @@
+package com.example.zaj261;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Zaj261Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Zaj261Application.class, args);
+	}
+}
---
+++ src/test/java/com/example/zaj261/Zaj261ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.zaj261;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Zaj261ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
