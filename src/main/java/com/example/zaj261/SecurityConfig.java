0370be8eebe4c9abec68a932851f51db826c465a
Agnieszka <gatito@poczta.pl>
2018-04-09 08:54:33 -0700

    prog

--- src/main/java/com/example/zaj261/SecurityConfig.java
+++ src/main/java/com/example/zaj261/SecurityConfig.java
@@ -1,14 +1,26 @@
 package com.example.zaj261;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Override
+/*    @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
+    } */
+@Override
+protected void configure(HttpSecurity http) throws Exception {
+    http.authorizeRequests().anyRequest().authenticated().and().formLogin();
+}
+    @Override
+    protected void configure (AuthenticationManagerBuilder auth) throws Exception {
+        UserDetails user = User.withDefaultPasswordEncoder().username ("user").password("123").roles("USER").build();
+        auth.inMemoryAuthentication().withUser(user);
     }
 }
