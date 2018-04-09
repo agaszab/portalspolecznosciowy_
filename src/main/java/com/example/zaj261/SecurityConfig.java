79ae4ad556d2466229668cb541b96e918fb974a7
Agnieszka <gatito@poczta.pl>
2018-04-09 10:31:36 -0700

    prog

--- src/main/java/com/example/zaj261/SecurityConfig.java
+++ src/main/java/com/example/zaj261/SecurityConfig.java
@@ -1,33 +1,51 @@
 package com.example.zaj261;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-/*    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
-    } */
-@Override
-protected void configure(HttpSecurity http) throws Exception {
-    http.authorizeRequests()
-            .antMatchers("/login").permitAll()
-            .antMatchers("/admin").hasRole("ADMIN")
-                    .anyRequest().authenticated().and()
-                    .formLogin()
-                        .loginPage("/login")
-    .and()
-    .csrf().disable();
-}
+    @Autowired
+    private DataSource dataSource;
+
+    /*    @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
+        } */
     @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/login").permitAll()
+                .antMatchers("/admin").hasRole("ADMIN")
+                .anyRequest().authenticated().and()
+                .formLogin()
+                .loginPage("/login");
+    }
+
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication()
+                .passwordEncoder(NoOpPasswordEncoder.getInstance())
+                .dataSource(dataSource)
+                .usersByUsernameQuery("select username, password, enabled from user where username=?")
+                .authoritiesByUsernameQuery("select username, role from user_role where username=?");
+
+//        UserDetails user = User.withDefaultPasswordEncoder().username("user").password("123").roles("ADMIN").build();
+//        auth.inMemoryAuthentication().withUser(user);
+    }
+   /* @Override
     protected void configure (AuthenticationManagerBuilder auth) throws Exception {
         UserDetails user = User.withDefaultPasswordEncoder().username ("user").password("123").roles("ADMIN").build();
         auth.inMemoryAuthentication().withUser(user);
-    }
+    } */
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
 insert into user (username, password, enabled) values ('admin', 'admin', true);
-insert into user (username, password, enabled) values ('admin', 'admin', true);
+insert into user (username, password, enabled) values ('user', 'user', true);
 
 insert into user_role (username, role) values ('admin', 'ROLE_ADMIN');
 insert into user_role (username, role) values ('user', 'ROLE_USER');
