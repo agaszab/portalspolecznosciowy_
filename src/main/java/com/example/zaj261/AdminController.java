b758cb22dc507cc23192f36031b36ad2ba829b43
agaszab <gatito@poczta.pl>
2018-04-12 15:19:32 +0200

    portal społeczniściowy

--- src/main/java/com/example/zaj261/AdminController.java
+++ src/main/java/com/example/zaj261/AdminController.java
@@ -3,6 +3,8 @@ package com.example.zaj261;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -16,14 +18,42 @@ public class AdminController {
     }
 
     @GetMapping("/admin")
-    public String adm (){
+    public String adm() {
         return "panel/admin";
     }
 
-    @GetMapping("/panel/users")
-    public String users (Model model){
-            List<User> users = userRepository.findAll();
-            model.addAttribute("users", users);
-    return "panel/users";
+    @GetMapping("panel/szukaj")
+    public String wyszukiwanie() {
+        return "panel/szukaj";
     }
+
+    @PostMapping("panel/szukaj")
+    public String wyszukaj(Model model, @RequestParam String username) {
+        User user = userRepository.findByUsername(username);
+        model.addAttribute("user", user);
+        return "panel/user";
+    }
+
+    @GetMapping("panel/user")
+    public String szukaj(Model model, @RequestParam String username) {
+        User user = userRepository.findByUsername(username);
+        model.addAttribute("user", user);
+        return "panel/user";
+    }
+    @GetMapping("/panel/users")
+    public String users(Model model) {
+        List<User> users = userRepository.findAll();
+        model.addAttribute("users", users);
+        return "panel/users";
+    }
+
+
+
+    @PostMapping("/edituser")
+    public String zmiendane(Model model, @RequestParam long id) {
+     return "panel/users";
+    }
+
+
 }
+
